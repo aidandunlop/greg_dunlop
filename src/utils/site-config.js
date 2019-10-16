a05e8d9742af4bf8388e11a2936e88333045ce40
Aidan Dunlop <aidandunlop@gmail.com>
2019-10-16 19:10:51 +0100

    clean

---
+++ src/utils/site-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteTitle: 'GREG DUNLOP',
+  siteTitleAlt: 'Greg Dunlop Artist',
+  publisher: 'Greg Dunlop', // Organization name used for SEO schema
+  siteDescription: 'Artist portfolio for Greg Dunlop.',
+  siteUrl: 'https://gregdunlop.com',
+  // postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
+  // postsPerPage: 6, // Number of posts shown on paginated pages
+  author: 'Greg Dunlop', // Author for RSS author segment and SEO schema
+  // authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+  // userTwitter: '@twitter', // Change for Twitter Cards
+  shortTitle: 'Greg Dunlop', // Used for App manifest e.g. Mobile Home Screen
+  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImageWidth: 900, // Change to the width of your default share image
+  shareImageHeight: 600, // Change to the height of your default share image
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
+  backgroundColor: '#e9e9e9', // Used for Offline Manifest
+  themeColor: '#121212', // Used for Offline Manifest
+  copyright: 'Copyright © 2019 Greg Dunlop', // Copyright string for the RSS feed
+};
